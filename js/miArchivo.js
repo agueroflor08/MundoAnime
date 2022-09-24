17ab59391c99129970da0bddfb8559cc72a4f6fa
Florencia Agüero <agueroflord8@gmail.com>
2022-09-24 19:27:10 -0300

    Archivos modificados 2

--- js/miArchivo.js
+++
@@ -1,7 +0,0 @@
-let cambio=window.document.getElementById('tama');
-cambio.addEventListener('input',cambiaFuente);
-
-function cambiaFuente () {
-    let miTexto=window.document.getElementById('parrafo');
-    miTexto.style.fontSize=cambio.value + "em";
-}
